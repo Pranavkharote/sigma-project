37e81203e0863e3ecdc9b07358c18040974053ae
Pranav kharote <pranavkharote2005@gmail.com>
2025-01-09 23:05:59 +0530

    updated again some UI changes

--- app.js
+++ app.js
@@ -60,10 +60,16 @@ const sessionOptions = {
   resave: false,
   saveUninitialized: true,
   cookie: {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
     expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
     maxAge: 7 * 24 * 60 * 60 * 1000,
-    httpOnly: true,
   },
+  // cookie: {
+  //   expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+  //   maxAge: 7 * 24 * 60 * 60 * 1000,
+  //   httpOnly: true,
+  // },
 };
 
 // app.get("/", (req, res) => {
