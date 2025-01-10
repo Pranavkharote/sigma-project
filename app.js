ee177b39abb4445ac694624602340d9a4bcf3b69
Pranav kharote <pranavkharote2005@gmail.com>
2025-01-10 11:10:32 +0530

    updated with some changes

--- app.js
+++ app.js
@@ -65,11 +65,6 @@ const sessionOptions = {
     expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
     maxAge: 7 * 24 * 60 * 60 * 1000,
   },
-  // cookie: {
-  //   expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-  //   maxAge: 7 * 24 * 60 * 60 * 1000,
-  //   httpOnly: true,
-  // },
 };
 
 // app.get("/", (req, res) => {
